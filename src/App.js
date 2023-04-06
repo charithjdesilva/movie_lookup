78a995b6487a88ba5f12498c3afac6a17128589b
charithjdesilva <charithjdesilva@gmail.com>
2023-04-06 10:29:14 +0530

    removed unnecessory movie1 object

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import MovieCard from './MoiveCard'
 
 const API_URL = 'http://www.omdbapi.com?apikey=b602e60'
 
-const movie1 = {
-    "Title": "Batman Begins",
-    "Year": "2005",
-    "imdbID": "tt0372784",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
-
 const App = () => {
     const [movies, setMovies] = useState([]);   //default value is an empty array
     const [searchTerm, setSearchTerm] = useState('');
