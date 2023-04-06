108f5ac9ed349b03fde94548305611bfb26237e3
charithjdesilva <charithjdesilva@gmail.com>
2023-04-06 09:46:45 +0530

    initalized the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com?apikey=b602e60'
+
+const movie1 = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
 }
 
-export default App;
+const App = () => {
+    const searchMovies = async (title) => {
+        const resopnse = await fetch(`${API_URL}&s=${title}`);
+        const data = await resopnse.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("batman");
+    },[]);
+
+    return (
+        <h1>Title</h1>
+    );
+}
+
+export default App
